4258d634105906b718c0760e63b9f2736aa5287b
DevUrielMC <urimarcervantes.95@gmail.com>
2019-10-23 15:39:11 -0500

    Finally

---
+++ src/com/exercise23generics/model/Suma.java
@@ -0,0 +1,18 @@
+package com.exercise23generics.model;
+
+public class Suma <T>
+
+
+	{
+		T miObjeto;
+
+		public Suma(T miObjeto)
+		{
+			this.miObjeto = miObjeto;
+		}
+
+		public T getMiObjeto()
+		{
+			return this.miObjeto;
+		}
+	}
